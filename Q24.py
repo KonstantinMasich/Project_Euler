1508645336bc2eda7d0c029d03666a861ca728df
Konstantin Masich <KonstantinMasich@outlook.com>
2017-10-21 00:38:34 -0700

    autocommit

--- Q24.py
+++ Q24.py
@@ -1,26 +1,31 @@
 # PROJECT EULER
-# Question 23
+# Question 24
 # Konstantin Masich
 # Python 3.5.2
+
+# Solved by math: T[i] = T[i-1] - row[i]*(n[i]-1)!
 
 import math
 
 def first_digit(l, target_num):
     """Gets list and target permutation num, returns 1st digit of perm."""
     index = target_num/math.factorial(len(l)-1)
-    return l[int(index)]
+    return int(index)
 
-#l = [0, 1, 2, 3]
-l = [1, 2, 3]
-n = len(l)
-target_permutation = l
-TARGET_PERMUTATION_NUMBER = 0
+l = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] # Starting set
+n = len(l)   # Length of starting set (will decrement over time)
+T = 999999   # Target permutation number - we begin counting FROM ZERO!
+res = []     # Result list
 
-stop_flag = False
-perm_no = 0
-
-for i in range(0, math.factorial(n)):
-    print("First digit is:", first_digit(l, i))
+while (n-1)>=0:
+    # 1. Getting current first digit:
+    row = first_digit(l, T)
+    x = l[row]
+    res.append(x)
+    # 2. Changing data:
+    T = int(T - row * math.factorial(n-1))
+    l.remove(x) # Working list
+    n = len(l)  # Working list size
     
-    
-print(int(14/6))
+# 3. Result is:
+print (''.join(str(x) for x in res))
