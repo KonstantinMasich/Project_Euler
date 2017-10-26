ea5a7b2092bdef12e73929aab82cec8aa0edbe67
KonstantinMasich <KonstantinMasich@outlook.com>
2017-10-26 00:51:04 -0700

    autocommit

--- Q25.py
+++ Q25.py
@@ -1,3 +1,6 @@
+import numpy as np
+import math
+
 def fibonacci(n):
         if n<3:
                 return 1
@@ -11,10 +14,15 @@ def fibo2(n):
     for i in range(1, n):
         F.append(F[len(F)-1] + F[len(F)-2])
     return F[len(F)-1]
-        
-        
-for i in range(1, 14):
-    print(i, fibo2(i))
     
-print(fibo2(1000))
-
+def digits_num(x):
+    """Counts digits in a number"""
+    x = np.float128(x)
+    return int(np.log10(x)+1)
+        
+        
+f = fibo2(1400)
+print(type(f))
+a = 8
+print(type(a))       
+print(digits_num(f))
